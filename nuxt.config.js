55804e28268322c934c13054ed42b04eee1c1f95
摆渡人 <1540035958@qq.com>
2022-10-22 00:35:43 +0800

    music fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Ferry-blog',
+    title: '摆渡人社区博客',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
