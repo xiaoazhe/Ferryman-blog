ced475f64e649d1b692643a1544b42ee58c8a340
摆渡人 <1540035958@qq.com>
2022-10-23 11:17:29 +0800

    恢复ip

--- utils/request.js
+++ utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {getUser} from '@/utils/auth'
 // 创建axios实例
 const service = axios.create({
-  baseURL: 'http://127.0.0.1:8010',
+  baseURL: 'http://121.5.129.126:8010',
   timeout: 10000, // 请求超时时间
   changeOrigin: true,
   headers: { 'Authorization': "Bearer " + getUser().token }
